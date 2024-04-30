6c13d0cc1bf90ee802af4a3924d37bac69b7e57e
dodotlm <tlma001003@gmail.com>
2024-04-30 10:00:05 +0900

    240429 homework 마태림_ advance1은 파일손실

--- _0429_advance3.java
+++ _0429_advance3.java
@@ -5,6 +5,7 @@ public class _0429_advance3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int r=0;
+		
 		for (int i =1; i <= 9; i+=2) {
 			for(int w = 1; w < 5-(i/2); w++) {
 				System.out.print(" ");
